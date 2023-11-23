13f8e5d0cf40d2611e023ff463eb822aec19602f
PauloCabral-hub <59887943+PauloCabral-hub@users.noreply.github.com>
2023-11-23 20:20:40 +0000

    updating

--- tools_for_text.sh
+++ tools_for_text.sh
@@ -11,3 +11,15 @@ sort <file1>
 
 # Interesting way to look at the last column of a .csv file
 cat <file1> | rev | cut -d, -f1 | rev
+
+# For very large files, you can use the following command to shuffle lines
+shuf -n 10 <file1>
+
+# For uppercase or lowercase strings
+tr a-z A-Z <file or string>
+
+# For making substitutions
+sed 's/<pattern_to_substitute>/<new_pattern>'
+
+# If you wanna see the results based on some condition, you can use the AWK language aid. Ex.:
+awk 'NF < 10' # shows the results if the number or results is less than 10
