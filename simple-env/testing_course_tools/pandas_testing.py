01108decba7d3cedd7268d954aebd9866ab0dc84
PauloCabral-hub <59887943+PauloCabral-hub@users.noreply.github.com>
2024-01-20 13:09:40 +0000

    updating

--- simple-env/testing_course_tools/pandas_testing.py
+++ simple-env/testing_course_tools/pandas_testing.py
@@ -17,4 +17,23 @@ df.columns=['subject','years','working_activity']
 
 # if we want to make a dataframe from a csv file
 file_path = '/workspaces/github-codespaces-dukecourse/simple-env/testing_course_tools/universidades.csv'
-pd.read_csv(file_path)
+df = pd.read_csv(file_path)
+
+# inspecting dataframe
+df.head() # by default, the argument is 5
+df.tai() 
+df.describe() # for statistics of your dataframe
+df.mean() # just one of the statistics
+df.std() # another
+
+# to show multiple columns
+df.columns(['col1', 'col2'])
+
+# to show a column named col1
+df.col1
+
+# acessing the data from iloc method
+df.iloc[0,0] # first row and first column respectively
+
+# with loc methods you can use the name of the column
+df.loc[0,['col1', 'col2']]
